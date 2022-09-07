7af896931381678d73d091baeef40cedc82553d0
Li Jiaqi <120090727@link.cuhk.edu.cn>
2022-09-07 21:38:12 +0800

    vga print

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -10,18 +12,9 @@ fn panic(_info: &PanicInfo) -> ! {
 
 static HELLO: &[u8] = b"Hello world";
 
+// Entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {
-        
-    }
+    vga_buffer::print_something();
+    loop {}
 }
